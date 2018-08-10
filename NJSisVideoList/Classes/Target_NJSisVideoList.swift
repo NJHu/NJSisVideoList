fe164b569e368da6096a38c7665f55614000745d
胡鹏 <64hp@163.com>
2018-08-10 22:11:55 +0800

    add

---
+++ NJSisVideoList/Classes/Target_NJSisVideoList.swift
@@ -0,0 +1,24 @@
+//
+//  Target_NJSisVideoList.swift
+//  NJSisVideoList
+//
+//  Created by HuXuPeng on 2018/8/10.
+//
+import UIKit
+import NJKit
+
+@objc class Target_NJSisVideoList: NSObject {
+    
+    @objc func Action_mainViewController(params: [String: AnyObject]) -> UIViewController? {
+        
+        let childController = NJSisVideoListController(tableViewStyle: .grouped)
+        let nav = NJNavigationController(rootViewController: childController)
+        
+        childController.tabBarItem.image = UIImage.nj_image(name: "tabBar_essence_icon", bundleClass: Target_NJSisVideoList.self)
+        childController.tabBarItem.selectedImage = UIImage.nj_image(name: "tabBar_essence_click_icon", bundleClass: Target_NJSisVideoList.self)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        childController.tabBarItem.title = "视频"
+        childController.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -3);
+        
+        return nav
+    }
+}
