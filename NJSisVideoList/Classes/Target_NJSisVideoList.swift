ad17d1e222f3633b8ccddae1c13d7b657d7466cc
胡鹏 <64hp@163.com>
2018-11-09 13:03:56 +0800

    update swift version -> 4.2

--- NJSisVideoList/Classes/Target_NJSisVideoList.swift
+++ NJSisVideoList/Classes/Target_NJSisVideoList.swift
@@ -15,7 +15,7 @@ import NJKit
         let nav = NJNavigationController(rootViewController: childController)
         
         childController.tabBarItem.image = UIImage.nj_image(name: "tabBar_essence_icon", bundleClass: Target_NJSisVideoList.self)
-        childController.tabBarItem.selectedImage = UIImage.nj_image(name: "tabBar_essence_click_icon", bundleClass: Target_NJSisVideoList.self)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        childController.tabBarItem.selectedImage = UIImage.nj_image(name: "tabBar_essence_click_icon", bundleClass: Target_NJSisVideoList.self)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         childController.tabBarItem.title = "视频"
         childController.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -3);
         
